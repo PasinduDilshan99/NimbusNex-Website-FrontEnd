028559b8a24cc887c3921153bd6ba91b1e3dad8c
Pasindu Dilshan Dimbulana <pasindudimbulana@gmail.com>
2024-09-07 23:11:57 +0530

    add some component to the about page

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import NavBar from '../components/navBar/NavBar'
 import TopImage from '../components/topImage/TopImage'
+import CompanyOverview from '../components/companyOverviewComponents/CompanyOverview'
+import Services from '../components/servicesComponents/Services'
+import ITSupport from '../components/itSupport/ITSupport'
+import FeedBack from '../components/feedback/FeedBack'
 
 const page = () => {
   return (
     <div>
       <NavBar/>
       <TopImage/>
+      <CompanyOverview/>
+      <Services/>
+      <ITSupport/>
+      <FeedBack/>
     </div>
   )
 }
