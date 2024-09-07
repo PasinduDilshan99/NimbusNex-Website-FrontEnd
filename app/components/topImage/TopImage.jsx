0b34b369ab62e0d30181fb9a7e7d52736cf72fa9
Pasindu Dilshan Dimbulana <pasindudimbulana@gmail.com>
2024-09-07 23:06:40 +0530

    add top image

--- app/components/topImage/TopImage.jsx
+++ app/components/topImage/TopImage.jsx
@@ -1,14 +1,29 @@
+import Image from "next/image";
 import React from "react";
 
 const TopImage = () => {
   return (
     <div
       style={{
-        backgroundImage: `url('/footer-bg.jpg')`,
+        backgroundImage: `url('/page-header-bg.jpg')`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
-    ></div>
+      className="relative min-h-[40vh]"
+    >
+      <div className="min-h-[30vh] flex flex-col items-center justify-center text-6xl text-white font-bold">
+        <div>ABOUT</div>
+        <div>Home / About</div>
+      </div>
+      <div className="absolute right-0 bottom-0 animate-move-left-right opacity-70"> 
+        <Image
+          src="/page-header-shape1.png"
+          alt="text"
+          height={500}
+          width={500}
+        />
+      </div>
+    </div>
   );
 };
 
