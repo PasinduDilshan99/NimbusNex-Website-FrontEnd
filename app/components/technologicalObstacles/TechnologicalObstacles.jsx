c4ce1e7bce4dc6b11afb77a123cb972818573d32
Pasindu Dilshan Dimbulana <pasindudimbulana@gmail.com>
2024-09-08 18:10:49 +0530

    update

--- app/components/technologicalObstacles/TechnologicalObstacles.jsx
+++ app/components/technologicalObstacles/TechnologicalObstacles.jsx
@@ -19,22 +19,25 @@ const TechnologicalObstacles = () => {
     >
       <div className="absolute inset-0 bg-gradient-to-t from-[#ffb237] to-transparent opacity-70 z-0" />
 
-      <div className="relative z-10">
-        <div className="flex">
+      <div className="relative z-10 py-4 w-full max-w-screen-lg">
+        <div className="flex gap-6 flex-wrap justify-center">
           {details.map((detail) => (
-            <div key={detail.id} className="flex">
+            <div
+              key={detail.id}
+              className="flex-1 min-w-[150px] sm:min-w-[200px] md:min-w-[250px] lg:max-w-[300px] h-32 flex items-center justify-center shadow-lg rounded-lg m-2"
+            >
               <TechnologicalObstaclesComponent props={detail} />
             </div>
           ))}
         </div>
 
-        <div className="text-black text-4xl font-bold mb-4 z-10">
+        <div className="text-black text-4xl font-bold mb-4 py-6 z-10">
           Assisting you in Overcoming your
           <br />
-          Technological 
+          Technological
         </div>
 
-        <div className="py-4 px-7 bg-black text-white text-xl mx-4 rounded-xl hover:bg-white hover:text-black transition-colors duration-1000 ease-in-out hover:scale-105 z-10">
+        <div className="py-6 px-7 bg-black text-white text-xl mx-4 rounded-xl hover:bg-white hover:text-black transition-colors duration-300 ease-in-out hover:scale-105 z-10">
           Discover more
         </div>
       </div>
