8077f195ddaed3774fe264e2096902e890a7df68
Pasindu Dilshan Dimbulana <pasindudi@mobitel.lk>
2024-09-17 14:42:18 +0530

    update the top slider in bugs

--- app/components/topSlider/TopSlider.jsx
+++ app/components/topSlider/TopSlider.jsx
@@ -60,63 +60,104 @@ const TopSlider = () => {
   };
 
   return (
-    <div   style={{ backgroundImage: `url(${currentChange.image})` }}>
     <div
-      className={` image-container w-full bg-cover  bg-center md:scale-110 py-24 ${
-        animateText ? "topSliderImageEntry md:topSliderImageZoom " : ""
-      }`}
+      className=""
       style={{ backgroundImage: `url(${currentChange.image})` }}
     >
-      <div className="flex justify-between items-center image-content">
+      <div
+        className={`relative image-container bg-cover bg-center py-24 ${
+          animateText ? "topSliderImageEntry md:topSliderImageZoom" : ""
+        }`}
+        style={{ backgroundImage: `url(${currentChange.image})` }}
+      >
+        <img
+          src="/slider-v3-shape4.png"
+          alt="background"
+          className={`absolute hidden lg:flex right-[40%] top-0 h-full w-full ${
+            animateText && "textComeFromBottomLeftLine"
+          }`}
+          style={{
+            opacity: 0.7,
+            zIndex: 2,
+            objectFit: "cover",
+            filter: "drop-shadow(0 4px 8px rgba(0, 0, 0, 1))",
+          }}
+        />
+
+        <img
+          src="/slider-v3-shape4.png"
+          alt="background"
+          className={`absolute hidden lg:flex right-[-20%] bottom-0 w-full h-full ${
+            animateText && "textComeFromRightBottomLine"
+          }`}
+          style={{
+            opacity: 0.7,
+            zIndex: 2,
+            objectFit: "cover",
+            filter: "drop-shadow(0 4px 8px rgba(0, 0, 0, 1))",
+          }}
+        />
+        {/* <div className="flex justify-center items-center"> */}
         <div
-          className={`hidden m-24 bg-white p-3 md:flex flex-col justify-center hover:bg-[#ffb237] rounded-xl transition-colors duration-3000 ease-in-out `}
+          className={`absolute right-[11%] bottom-[20%] x text-center ${
+            animateText && "textComeFromRightBottomLine"
+          }`}
         >
-          <button onClick={handlePrevious}>
-            <ArrowBackIosIcon />
-          </button>
+          <h1 className={`hidden xl:flex rotate-[-51deg] text-center text-3xl`}>Ready to transform your business? IT Profestional Development</h1>
         </div>
-        <div className={`flex flex-col py-6 `}>
-          <div className="flex justify-center">
+
+        {/* </div> */}
+
+        <div className="flex  max-w-[100%] items-center justify-between image-content">
+          <div
+            className={`hidden  ml-[10%] mr-24 bg-white p-3 md:flex flex-col justify-center hover:bg-[#ffb237] rounded-xl transition-colors duration-3000 ease-in-out `}
+          >
+            <button onClick={handlePrevious}>
+              <ArrowBackIosIcon />
+            </button>
+          </div>
+          <div className={` flex flex-col py-6 `}>
+            <div className="flex justify-center ">
+              <div
+                className={`bg-white p-3 rounded-full hover:bg-[#ffb237] transition-colors duration-1000 ease-in-out ${
+                  animateText ? "text-top-animate" : ""
+                }`}
+              >
+                <Image
+                  src="/company-logo.png"
+                  width={30}
+                  height={30}
+                  alt="logo"
+                />
+              </div>
+            </div>
             <div
-              className={`bg-white p-3 rounded-full hover:bg-[#ffb237] transition-colors duration-1000 ease-in-out ${
+              className={`text-4xl md:text-5xl lg:text-7xl capitalize  flex my-8 px-4 justify-center content-center text-center font-bold text-white hover:text-amber-500 transition-colors duration-1000 ease-in-out ${
                 animateText ? "text-top-animate" : ""
               }`}
             >
-              <Image
-                src="/company-logo.png"
-                width={30}
-                height={30}
-                alt="logo"
-              />
+              {currentChange.text}
+            </div>
+            <div
+              className={`flex flex-col lg:flex-row justify-center items-center text-2xl py-4 grow ${
+                animateText ? "text-bottom-animate" : ""
+              }`}
+            >
+              <div className="p-4 mx-4 ">
+                <WhiteBottomToTop props={{ text: "  View Our Services" }} />
+              </div>
+              <div className="p-4 ">
+                <YellowBottomToTop props={{ text: "Learn More" }} />
+              </div>
             </div>
           </div>
-          <div
-            className={`text-4xl md:text-5xl lg:text-7xl capitalize flex my-8 px-4 justify-center content-center text-center font-bold text-white hover:text-amber-500 transition-colors duration-1000 ease-in-out ${
-              animateText ? "text-top-animate" : ""
-            }`}
-          >
-            {currentChange.text}
+          <div className="hidden  md:flex mr-[10%] ml-24 bg-white p-3 hover:bg-[#ffb237] rounded-xl transition-colors duration-1000 ease-in-out">
+            <button onClick={handleNext}>
+              <ArrowForwardIosIcon />
+            </button>
           </div>
-          <div
-            className={`flex flex-col lg:flex-row justify-center items-center text-2xl py-4 grow ${
-              animateText ? "text-bottom-animate" : ""
-            }`}
-          >
-            <div className="p-4 mx-4 ">
-              <WhiteBottomToTop props={{ text: "  View Our Services" }} />
-            </div>
-            <div className="p-4 ">
-              <YellowBottomToTop props={{ text: "Learn More" }} />
-            </div>
-          </div>
-        </div>
-        <div className="hidden md:flex m-24 bg-white p-3 hover:bg-[#ffb237] rounded-xl transition-colors duration-1000 ease-in-out">
-          <button onClick={handleNext}>
-            <ArrowForwardIosIcon />
-          </button>
         </div>
       </div>
-    </div>
     </div>
   );
 };
